eec7537e3d04950e567d4b4f02878d9cb2aec98d
shady842 <95432020+shady842@users.noreply.github.com>
2021-12-06 23:06:37 +0400

    Add project files.

---
+++ leavemanagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace leavemanagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
